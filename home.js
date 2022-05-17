dde0c8b53b98ae159255f881c729230969343272
sexbavip <105713311+sexbavip@users.noreply.github.com>
2022-05-17 17:42:09 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,23 @@
+(function(w) {
+    function enterLine() {
+        document.getElementById("btnEnter1").onclick = function() {
+            window.open('http://32.321fds3g213df2.cc', "_blank");
+        }
+        document.getElementById("btnEnter2").onclick = function() {
+            window.open('http://1h.1h32fd1g32j1d3.cc', "_blank");
+        }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('http://21.21jf3g51b23v1n.xyz', "_blank");
+        }
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('http://32.321nm321h5jk46.xyz', "_blank");
+        }
+        document.getElementById("btnEnter5").onclick = function() {
+            window.open('http://54.54yu8t7yu9r8t7.info', "_blank");
+        }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);
