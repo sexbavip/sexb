84f45e6a1e9c3d51813bee54cb165cb6d4f147a9
sexbavip <105713311+sexbavip@users.noreply.github.com>
2022-08-11 15:45:54 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://api.de1n6jgc.club', "_blank");
+            window.open('https://api.f5r8lhch.life', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://api.xwo5gkpp.world', "_blank");
+            window.open('https://api.ovxn1d8h.club', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://api.de1n6jgc.club', "_blank");
+            window.open('https://api.f5r8lhch.life', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://api.xwo5gkpp.world', "_blank");
+            window.open('https://api.ovxn1d8h.club', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('https://api.de1n6jgc.club', "_blank");
+            window.open('https://api.f5r8lhch.life', "_blank");
         }
     }
     function init() {
